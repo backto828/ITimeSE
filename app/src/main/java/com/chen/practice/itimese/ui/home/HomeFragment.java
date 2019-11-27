ba00ee939a552763b72ffe44423c85708bee1b69
backto828 <411532139@qq.com>
2019-11-27 21:29:49 +0800

    ListView

--- app/src/main/java/com/chen/practice/itimese/ui/home/HomeFragment.java
+++ app/src/main/java/com/chen/practice/itimese/ui/home/HomeFragment.java
@@ -1,16 +1,14 @@
 package com.chen.practice.itimese.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.chen.practice.itimese.R;
 import com.chen.practice.itimese.model.MyTicker;
@@ -20,8 +18,17 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
+import static android.app.Activity.RESULT_OK;
+
 public class HomeFragment extends Fragment {
 
+    // 定义一些整型常量
+    private static final int REQUEST_CODE = 1500;
+    private static final int ADD_MODE = 1;
+    private static final int MODIFY_MODE = 2;
+    private static final int DELETE_MODE = 3;
+
+    // 数组及适配器
     private ArrayList<MyTicker> myTickers;
     private MyTickerAdapter myTickerAdapter;
 
@@ -32,6 +39,10 @@ public class HomeFragment extends Fragment {
 
         // 定义数据
         initData();
+        // ListView
+        ListView listViewMyTimer = root.findViewById(R.id.list_view_my_timer);
+        listViewMyTimer.setAdapter(myTickerAdapter);
+
         // 新增按钮
         FloatingActionButton fab = root.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,9 +56,46 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+    //接收并添加传过来的值
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == HomeFragment.REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                int mode = bundle.getInt("mode");
+                switch (mode) {
+                    case ADD_MODE: {
+                        MyTicker myTicker = (MyTicker) bundle.getSerializable("time");
+                        myTickers.add(myTicker);
+                        myTickerAdapter.notifyDataSetChanged();
+                        break;
+                    }
+                    case MODIFY_MODE: {
+                        MyTicker myTicker = (MyTicker) bundle.getSerializable("time");
+                        int position = bundle.getInt("position");
+                        if (position >= 0 && position < myTickers.size()) {
+                            myTickers.set(position, myTicker);
+                            myTickerAdapter.notifyDataSetChanged();
+                        }
+                        break;
+                    }
+                    case DELETE_MODE: {
+                        int position = bundle.getInt("position");
+                        if (position >= 0 && position < myTickers.size()) {
+                            myTickers.remove(position);
+                            myTickerAdapter.notifyDataSetChanged();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     private void initData() {
         // 加载myTime 等待重构
-//        myTickers = MyTimeManager.load(this.getContext());
-//        myTickerAdapter = new MyTickerAdapter(this.getContext(), R.layout.home_my_time_item_layout, myTickers);
+        // myTickers = MyTimeManager.load(this.getContext());
+        // myTickerAdapter = new MyTickerAdapter(this.getContext(), R.layout.home_my_time_item_layout, myTickers);
     }
 }
